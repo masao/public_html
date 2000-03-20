89b526f0e679baf4a5473ae390329f818f9310c1
masao <tmasao@acm.org>
2000-03-20 16:05:39 +0000

    add files

---
+++ ipc-search/update.pl
@@ -0,0 +1,58 @@
+#!/usr/local/bin/perl -w
+# $Id$
+
+use strict;
+$| = 1;
+
+######グローバル変数 BEGIN
+	#コマンド
+my $Wget='/usr/local/bin/wget';
+my $Mknmz='/usr/local/bin/mknmz';
+
+	#ディレクトリ
+my $BaseDir="/home/masao/Namazu/ipc-search";
+my $HtmlDir="/home/masao/public_html/ipc-search";
+
+	#その他
+my $TargetUrl="http://www.ulis.ac.jp/ipc/ http://www.ulis.ac.jp/newsys/";
+######グローバル変数 END
+
+if (-d $BaseDir) {
+    chdir($BaseDir);
+} else {
+    die "ERROR: 作業ディレクトリ $BaseDir がありません。";
+}
+
+# 以前のファイル群は(一応)バックアップしておく。
+if (-d "www.ulis.ac.jp") {
+    print "古いファイルをバックアップします。... ";
+    system "rm -rf www.ulis.ac.jp.old";
+    system "mv -f www.ulis.ac.jp www.ulis.ac.jp.old";
+    print "完了。\n";
+}
+
+# まず、ページを収集する。
+print "文書群を収集します。... ";
+system "$Wget --mirror --no-parent -l 0 -R .gif,.GIF,.jpg,.JPG,.jpeg,.avi,.mov,.mpg,.mpeg,.pic,.pict,.ppm,.tiff,.tiff,.vrml,.wrl,.xpm,.ps,.aif,.au,.cdr,.hcom,.mid,.pcm,.ra,.ram,.smp,.snd,.wav,.wave,.hqx,.lzh,.sit,.tar,.tgz,.zip,.exe,.class --wait=5 --proxy=off --output-file=wget.log $TargetUrl" ;
+print "完了。\n";
+
+# 次に、Indexingする。
+print "Indexing を行います。\n";
+if (-d "www.ulis.ac.jp") {
+    $ENV{'LANG'} = "ja_JP.EUC";
+    system "$Mknmz --all --checkpoint --use-chasen --replace='s#${BaseDir}/#http://#;' ${BaseDir}/www.ulis.ac.jp/";
+} else {
+    die "収集した文書が $BaseDir/www.ulis.ac.jp にありません。";
+}
+print "完了。\n";
+
+# 最後にトップページを自動的に更新させる。
+print "トップページを更新します。... ";
+if (-f "$BaseDir/NMZ.head.ja" &&
+    -f "$HtmlDir/body.txt" &&
+    -f "$BaseDir/NMZ.foot.ja") {
+    system "cat $BaseDir/NMZ.head.ja $HtmlDir/body.txt $BaseDir/NMZ.foot.ja > $HtmlDir/index.html";
+    print "完了\n";
+} else {
+    warn "必要なファイルが見つかりません。";
+}
