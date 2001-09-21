a872274dd39a8cce7924ad62e60f17642e32b057
masao <tmasao@acm.org>
2001-09-21 20:26:26 +0000

    Sleep one second between fetching URLs. Add copyright notice.

--- software/google-cache.pl
+++ software/google-cache.pl
@@ -1,5 +1,23 @@
 #!/usr/local/bin/perl -w
 # $Id$
+#
+#  Copyright (C) 2001 Masao Takaku <masao@ulis.ac.jp>.
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# This file must be encoded in EUC-JP.
 
 use strict;
 use FileHandle;
@@ -59,6 +77,7 @@ sub main {
 	# さらなるリンクを探す
 	$cont =~ s#\s+##g;
 	$cont =~ s/href=([\"\']?)([^\>]+?)\1/register_uri($2, $uri, $level)/gei;
+	sleep 1;
     }
 }
 
