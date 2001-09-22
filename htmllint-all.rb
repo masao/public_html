eb850a7d0b25b59cbe5d40f5a5264b5379533a78
masao <tmasao@acm.org>
2001-09-22 13:59:03 +0000

    Fix serious bug that lint is not executed to files recorded in score file.

--- htmllint-all.rb
+++ htmllint-all.rb
@@ -9,27 +9,29 @@ require "find"
 files = {}
 scorefile = "score"
 open(scorefile).each do |line|
-  if line =~ /^(\S+)\s+(.*)$/ then
-    files[$2] = $1
-  end
+   if line =~ /^(\S+)\s+(.*)$/ then
+      files[$2] = $1
+   end
 end
 
 last_time = File.mtime(scorefile)
+
+File.rename(scorefile, scorefile+".bak")	# Backup
 out = open(scorefile, "w")
 target = []
 Find::find('.') do |f|
-  if f =~ /\.html?$/i then
-    if not files.key? f || File.mtime(f) > last_time
-      target.push f
-    else
-      score = files[f]
-      out.puts "#{score} #{f}"
-    end
-  end
+   if f =~ /\.html?$/i
+      if files.key?(f) && File.mtime(f) < last_time
+	 score = files[f]
+	 out.puts "#{score} #{f}"
+      else
+	 target.push f
+      end
+   end
 end
 
 target.each do |f|
-  score = `htmllint -scoreonly -nobanner -nowarnings #{f}`
-  puts "#{f} => #{score}"
-  out.puts "#{score} #{f}"
+   score = `htmllint -scoreonly -nobanner -nowarnings #{f}`
+   puts "#{f} => #{score}"
+   out.puts "#{score}\t#{f}"
 end
