1ff02838e037d15f5b100958063ba121fc10f41e
masao <tmasao@acm.org>
2003-04-23 16:16:45 +0000

    -n オプションを追加。 変換前のファイル名も \nn 形式で表示するようにした。

--- software/rename_ppt_imgs.pl
+++ software/rename_ppt_imgs.pl
@@ -5,11 +5,22 @@
 # （Unix上などで扱いやすいように）変更する。
 #   「スライド1.GIF」→「001.gif」
 
+### Usage: rename_ppt_imgs.pl [-n]
+
 use strict;
 
 # 半角カナ: "\x8e\x57\x8e\x44\x8e\x5e";
 # 全角カナ: "\x83\x58\x83\x89\x83\x43\x83\x68";
 my $SURAIDO = "(\x83\x58\x83\x89\x83\x43\x83\x68|\x8e\x57\x8e\x44\x8e\x5e)";
+
+my $NOCHANGE = 0;
+
+while (defined $ARGV[0]) {
+    my $opt = shift @ARGV;
+    if ($opt eq "-n") {
+	$NOCHANGE = 1;
+    }
+}
 
 opendir(DIR, ".") || die "opendir fail: $!";
 my @file = readdir(DIR);
@@ -19,7 +30,15 @@ foreach my $f (@file) {
 	$newname =~ s/^$SURAIDO//;
 	$newname =~ s/(\.\w+)$/lc($1)/e;
 	$newname =~ s/(\d+)/sprintf("%03d", $1)/e;
-	print "rename to $newname\n";
-	rename($f, $newname) || die "rename failed: $!";
+	unless ($NOCHANGE) {
+	    rename($f, $newname) || die "rename failed: $!";
+	}
+	print escape($f) ." -> ". escape($newname) ."\n";
     }
+}
+
+sub escape($) {
+    my ($str) = @_;
+    $str =~s/([^a-zA-Z0-9_.-])/sprintf("\\%03o",ord($1))/eg;
+    return $str;
 }
