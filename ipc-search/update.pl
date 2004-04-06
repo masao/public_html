1d67c8ae8efa1c4eac20e1b754a7666211e70cff
masao <tmasao@acm.org>
2004-04-06 20:41:44 +0000

    settime.pl 相当をサブルーチン化 ulis.ac.jp → slis.tsukuba.ac.jp 対応

--- ipc-search/update.pl
+++ ipc-search/update.pl
@@ -2,6 +2,10 @@
 # $Id$
 
 use strict;
+use IO::File;
+use File::Find;
+use Digest::MD5;
+
 $| = 1;
 
 ######グローバル変数 BEGIN
@@ -25,28 +29,28 @@ sub main {
     }
 
     # 以前のファイル群は(一応)バックアップしておく。
-    backup_dirs("www.ulis.ac.jp", "www.cc.tsukuba.ac.jp");
+    backup_dirs("www.slis.tsukuba.ac.jp", "www.cc.tsukuba.ac.jp");
 
     # まず、ページを収集する。
     print "文書群を収集します。... ";
-    system "$Wget $WgetOpt --output-file=wget.log -I ipc -I newsys http://www.ulis.ac.jp/ipc/" ;
+    system "$Wget $WgetOpt --output-file=wget.log -I ipc -I newsys -I subnet http://www.slis.tsukuba.ac.jp/ipc/" ;
     system "$Wget $WgetOpt --append-output=wget.log --no-parent http://www.cc.tsukuba.ac.jp/kasuga/";
     print "完了 - " . `date` ."\n";
 
-    # 収集したページのうち、LastModifiedヘッダを返さないページの更新日付を
-    # 調整する。
-    print "ファイルの更新日付を調整します。... ";
-    system "$HtmlDir/settime.pl";
-    print "完了 - " . `date` ."\n";
-
-    # 次に、Indexingする。
-    print "Indexing を行います。\n";
-    if (-d "www.ulis.ac.jp" &&
+    if (-d "www.slis.tsukuba.ac.jp" &&
 	-d "www.cc.tsukuba.ac.jp") {
+	# 収集したページのうち、LastModifiedヘッダを返さないページの更新日付を
+	# 調整する。
+	print "ファイルの更新日付を調整します。... ";
+	settime("www.slis.tsukuba.ac.jp", "www.cc.tsukuba.ac.jp");
+	print "完了 - " . `date` ."\n";
+
+	# 次に、Indexingする。
+	print "Indexing を行います。\n";
 	$ENV{'LANG'} = "ja";
 	system "$Mknmz --all --checkpoint -f ${HtmlDir}/mknmzrc --replace='s#${BaseDir}/#http://#;' *.ac.jp";
     } else {
-	die "収集した文書が $BaseDir/www.ulis.ac.jp にありません。";
+	die "収集した文書が $BaseDir/www.slis.tsukuba.ac.jp にありません。";
     }
     print "完了 - " . `date` ."\n";
 
@@ -72,8 +76,8 @@ sub main {
 sub get_loginfo($) {
     my ($file) = @_;
     my %info = ();
-    open(LOG, $file) || die "can't open $file: $!";
-    my @tmp = <LOG>;
+    my $fh = fopen($file);
+    my @tmp = <$fh>;
     @tmp = reverse @tmp;
     for my $line (@tmp) {
 	last if ($line =~ /^\[/);
@@ -93,21 +97,21 @@ sub update_news($%) {
 		       (localtime)[4]+1,
 		       (localtime)[3]);
 
-    open(HTML, $file) || die "can't open $file: $!";
-    my @tmp = <HTML>;
-    close(HTML);
+    my $fh = fopen($file);
+    my @tmp = <$fh>;
+    undef $fh;
 
-    open(HTML, ">$file") || die "can't open $file: $!";
+    $fh = fopen(">$file");
     for my $line (@tmp) {
 	if ($line =~ /$flag/i) {
-	    print HTML "$flag\n";
-	    print HTML "  <dt>$date\n";
-	    print HTML "  <dd>". info2str(%info);
+	    print $fh "$flag\n";
+	    print $fh "  <dt>$date\n";
+	    print $fh "  <dd>". info2str(%info);
 	} else {
-	    print HTML $line;
+	    print $fh $line;
 	}
     }
-    close(HTML);
+    undef $fh;
 }
 
 sub info2str(%) {
@@ -134,4 +138,63 @@ sub backup_dirs(@) {
 	}
     }
     print "完了 - " . `date` ."\n";
+}
+
+sub settime(@) {
+    my (@dirs) = @_;
+    my %files = ();
+
+    my $file = "$HtmlDir/digest";
+    if (my $fh = fopen($file)) {
+	while (<$fh>) {
+	    chomp;
+	    my ($path, $info) = split(/\t/, $_, 2);
+	    $files{$path} = $info;
+	}
+    }
+
+    my $wanted = sub {
+	my $f = $_;
+	return unless -f $f;
+	my $name = $File::Find::name;
+	my @stat = stat($f);
+	my $mtime = $stat[9];
+	my $size = $stat[7];
+	my $digest = md5_file($f);
+	if (defined($files{$name})) {
+	    my ($mtime_old, $size_old, $digest_old) = split(/\t/, $files{$name});
+	    if ($size == $size_old && $digest eq $digest_old) {
+		$mtime = $mtime_old;
+		utime(time, $mtime, $f);
+	    }
+	}
+	$files{$name} = join("\t", $mtime, $size, $digest);
+    };
+    find($wanted, @dirs);
+
+    my $fh = fopen(">$file.new");
+    foreach my $f (sort keys %files) {
+	print $fh "$f\t$files{$f}\n";
+    }
+    undef $fh;
+
+    if (-e $file) {
+	rename($file, "$file.old") || die "Can't rename $file";
+    }
+    rename("$file.new", $file) || die "Can't rename $file.new";
+}
+
+sub md5_file($) {
+    my ($file) = @_;
+    my $md5 = Digest::MD5->new;
+    my $fh = fopen($file);
+    return $md5->addfile($fh)->hexdigest;
+}
+
+sub fopen($) {
+    my ($file) = @_;
+    my $fh = new IO::File;
+    $fh->open($file) || die "$file: $!";
+    $fh->autoflush(1);
+    return $fh;
 }
