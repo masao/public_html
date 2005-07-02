044e689b977f2c54136ea939fedf49b94a83fd39
masao <tmasao@acm.org>
2005-07-02 12:52:49 +0000

    introduce new method: def bsfilter_probs(io)

--- software/bs-tokens.rb
+++ software/bs-tokens.rb
@@ -4,29 +4,34 @@
 # bsfilter は Paul Graham メソッドでない場合は、
 # スパム確率に寄与したトークンをうまく出力しないので自前で…。
 
-require 'getopts'
-
-$max = 20
-
-getopts("m:", "max:")
-if $OPT_m or $OPT_max
-   $max = ($OPT_m or $OPT_max).to_i
+def bsfilter_probs(io)
+   probs = {}
+   data = io.readlines
+   if data.size > 0
+      probs["all"] = data.pop.split[4].to_f
+      probs["word"] = data.find_all{|e|
+         /^word probability\s/ =~ e
+      }.map{|e|
+         e.split[2..-1]
+      }.sort{|a,b|
+         (0.5 - b[-1].to_f).abs - (0.5 - a[-1].to_f).abs
+      }
+      probs
+   else
+      nil
+   end
 end
 
-ARGV.each do |f|
-   IO.popen("bsfilter -d #{f}") do |io|
-      data = io.readlines
-      if data.size > 0
-	 data.collect{|e|
-	    e.split
-	 }.find_all{|e|
-	    e[5]
-	 }.sort{|a,b|
-	    (0.5 - b[5].to_f).abs - (0.5 - a[5].to_f).abs
-	 }[0..$max-1].each{|e|
-	    puts e.join(" ")
-	 }
-	 puts data[-1]
+if $0 == __FILE__
+   require 'getopts'
+   getopts("m:", "max:")
+   max = 20
+   max = ($OPT_m or $OPT_max).to_i if $OPT_m or $OPT_max
+   ARGV.each do |f|
+      IO.popen("bsfilter -d #{f}") do |io|
+         prob = bsfilter_probs(io)
+         puts prob["word"][0..max-1].map{|e| e.join(" ") }
+         puts prob["all"]
       end
    end
 end
