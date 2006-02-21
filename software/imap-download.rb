20c6133d0caf659dae4aa732419b70859019063a
masao <tmasao@acm.org>
2006-02-21 16:15:43 +0000

    SSL対応追加 logout追加

--- software/imap-download.rb
+++ software/imap-download.rb
@@ -8,23 +8,24 @@ require 'ftools'
 
 File.umask(0077)	# 所有者以外は見れないように。。。
 
-HOST	 = "mp.nii.ac.jp"
+HOST	 = "tachibana.slis.tsukuba.ac.jp"
 UID	 =  "masao"
 PASSWORD = "XXXXXXXXX"
 
-USE_SSL = false # true
+USE_SSL =  true # false
 PORT = USE_SSL ? 993 : 143
 
 OUTDIR	 = "/tmp/#{UID}-#{HOST}"
 File.makedirs OUTDIR unless File.directory? OUTDIR
 
 #Net::IMAP.debug = true
-imap = Net::IMAP.new(HOST)
+imap = Net::IMAP.new(HOST, PORT, USE_SSL)
 imap.login(UID, PASSWORD)
 folder = imap.list("", "*")
 folder.each do |d|
    dir = d.name
-   imap.select(dir)
+   imap.examine(dir)
+   next if imap.responses["EXISTS"][-1] == 0
    File.makedirs "#{OUTDIR}/#{dir}" unless File.directory? "#{OUTDIR}/#{dir}"
    data = imap.fetch(1..-1, "RFC822")
    data.each do |m|
@@ -35,4 +36,5 @@ folder.each do |d|
       puts fname
    end
 end
+imap.logout
 imap.disconnect
