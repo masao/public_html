3b84d566cbaf088808389f60a18b81337d675bc2
masao <tmasao@acm.org>
2006-04-27 10:30:31 +0000

    html化

--- pub/index.rb
+++ pub/index.rb
@@ -3,13 +3,31 @@
 
 require "yaml"
 require "cgi"
+require "erb"
 
-puts "Content-Type: text/plain"
-puts
+class CGI
+   attr_accessor :lang, :tmpl
+end
+
+DEFAULT_LANG = "ja"
+PUBDATA = "pub.yaml"
+LASTUPDATE = File::mtime( PUBDATA )
+
+cgi = CGI::new
+cgi.lang = DEFAULT_LANG
+cgi.tmpl = "pub.rhtml.#{cgi.lang}"
+
+print cgi.header("text/html; charset=UTF-8")
 
 pubs = []
-YAML::load_documents(open("pub.yaml")){|e| pubs << e }
+YAML::load_documents(open(PUBDATA)){|e| pubs << e }
 #p pubs
-pubs.sort_by{|e| e["year"] }.each do |pub|
-   puts pub.to_yaml
+
+sort_mode = :year
+pubs.select{|e| not e.nil? }.sort_by do |e|
+   e[sort_mode.to_s]
 end
+all_keys = pubs.map{|e| e[sort_mode] }.uniq.sort
+all_keys.reverse! if sort_mode == :year
+
+puts result = ERB::new( open("pub.rhtml.ja"){|f| f.read } ).result( binding )
