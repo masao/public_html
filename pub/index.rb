32f3c902dba70007be8f8fe591d154f24a079364
masao <tmasao@acm.org>
2006-04-27 15:38:48 +0000

    fix a bug that cannot sort correctly

--- pub/index.rb
+++ pub/index.rb
@@ -7,13 +7,22 @@ require "erb"
 require "rexml/document"
 #require "yaml"
 
-class CGI
+class PubList
    attr_accessor :lang, :tmpl
+   attr_reader :cgi
+
+   def initialize
+      @cgi = CGI.new
+   end
+   def header(arg)
+      @cgi.header(arg)
+   end
+   
    SORT_ACCEPT = [:year, :type, :author]
    SORT_DEFAULT = SORT_ACCEPT[0]
    def sort_mode
-      if self.params["sort_mode"][0] and self.params["sort_mode"][0].size > 0
-         mode = self.params["sort_mode"][0].intern
+      if @cgi.params["sort_mode"][0] and @cgi.params["sort_mode"][0].size > 0
+         mode = @cgi.params["sort_mode"][0].intern
          if SORT_ACCEPT.member? mode
             mode
          else
@@ -23,25 +32,34 @@ class CGI
          SORT_DEFAULT
       end
    end
+   def get_sort_key(element)
+      case self.sort_mode
+      when :type
+         element.attributes["type"].text
+      else
+         element.elements[self.sort_mode.to_s].text
+      end
+   end
 end
 
 DEFAULT_LANG = "ja"
 PUBDATA = "pub.xml"
 LASTUPDATE = File::mtime( PUBDATA )
 
-cgi = CGI::new
+cgi = PubList::new
 cgi.lang = DEFAULT_LANG
 cgi.tmpl = "pub.rhtml.#{cgi.lang}"
 
 print cgi.header("text/html; charset=UTF-8")
 
 pubs = REXML::Document.new(open(PUBDATA)).elements.to_a("/publist/pub")
-pubs.sort_by do |e|
-   e.elements[cgi.sort_mode.to_s].text
+pubs = pubs.sort_by do |e|
+   p cgi.get_sort_key(e)
+   cgi.get_sort_key(e)
 end
 
 # pubs << e.elements[cgi.sort_mode.to_s]
-toc_keys = pubs.map{|e| e.elements[cgi.sort_mode.to_s].text }.uniq.sort
+toc_keys = pubs.map{|e| cgi.get_sort_key(e) }.uniq.sort
 toc_keys.reverse! if cgi.sort_mode == :year
 
-puts result = ERB::new(open(cgi.tmpl){|f|f.read}, $SAFE, 2).result( binding )
+print result = ERB::new(open(cgi.tmpl){|f|f.read}, $SAFE, 2).result( binding )
