d8e2757af3e9268cc221402b6d2a24c56aec2f30
masao <tmasao@acm.org>
2007-10-26 09:45:30 +0000

    ID datatype does'nt allow num-chars for the first char.

--- d/delicious.js
+++ d/delicious.js
@@ -1,5 +1,5 @@
 function delicious_totalposts(data) {
     var urlinfo = data[0];
     if (!urlinfo.total_posts) return;
-    document.getElementById(urlinfo.hash +"_totalposts").innerHTML = urlinfo.total_posts + " others";
+    document.getElementById("totalposts_" + urlinfo.hash).innerHTML = urlinfo.total_posts + " others";
 }
