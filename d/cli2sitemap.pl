af4a1e539b50901d439d1f6e294a059edf2dd841
masao <tmasao@acm.org>
2008-03-13 09:24:51 +0000

    sitemaps.org

--- d/cli2sitemap.pl
+++ d/cli2sitemap.pl
@@ -52,7 +52,7 @@ URL
 
 print <<XML;
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.google.com/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
    <url>
       <loc>$BASEURI</loc>
       <changefreq>daily</changefreq>
