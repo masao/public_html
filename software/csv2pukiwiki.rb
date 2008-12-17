7f285ff3d18a171882fae45dbd2f1ba532083340
masao <tmasao@acm.org>
2008-12-17 02:05:53 +0000

    For dlwiki

---
+++ software/csv2pukiwiki.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# -*- coding: euc-jp -*-
+# $Id$
+
+require "csv"
+# require "kconv"
+
+PAGE_TEMPLATE = <<EOF
+* %%DATA7%%
+-ページ: [[Issue]]
+-報告者: [[%%DATA5%%]]
+-状態: %%DATA3%%
+-カテゴリー: %%DATA2%%
+-報告日: %%DATA4%%
+-対応予定日: %%DATA11%%
+-報告メール: %%DATA6%%
+
+**メッセージ
+%%DATA8%%
+
+%%DATA9%%
+
+(解決策)
+%%DATA10%%
+
+----
+#comment
+EOF
+PAGE_NAME = "Issue/%%DATA1%%"
+
+class String
+   def expand_vars( data_row )
+      self.gsub( /%%DATA(\d+)%%/ ) do |matched|
+         colnum = $1.to_i
+         data_row[colnum - 1]
+      end
+   end
+end
+
+csvreader = CSV::IOReader.new( ARGF )
+csvreader.each do |data|
+   # p data
+   content = PAGE_TEMPLATE.expand_vars( data )
+   pagename = PAGE_NAME.expand_vars( data ).unpack("H*0").join.upcase + ".txt"
+   open( "wiki/#{pagename}", "w" ) do |io|
+      io.print content
+   end
+   sleep 1
+end
