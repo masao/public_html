7103e6629bc034af52d83d53a4aef0d267c1855c
masao <tmasao@acm.org>
2009-05-05 02:47:31 +0000

    rm debug print

--- software/cl2bicycle-meter.pl
+++ software/cl2bicycle-meter.pl
@@ -18,7 +18,7 @@ foreach my $ymd (sort keys %{$cl->{all}}) {
     for (my $i = $entry->{curid}; $i >= 1; $i--) {
 	if ($entry->{$i}->{ho} eq '今日のサイクリング') {
 	    my $cont = $entry->{$i}->{co};
-	    print join( "\t", "<$ymd>", $cont );
+	    #print join( "\t", "<$ymd>", $cont );
 	    my $dist = $1 if $cont =~ /距離:\s*([\d\.]+)\s*km/iso;
 	    my $time = $1 if $cont =~ /時間:\s*([\d\.\:]+)/iso;
 	    $time = "0:".$time if not defined((split(/:/, $time))[2]);
