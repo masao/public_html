974f9eb86d3bd732c6aa01c8c031c3b3571791da
masao <tmasao@acm.org>
2009-05-13 06:40:38 +0000

    累積メータもプロットするよう修正

--- d/bicycle-meter-gnuplot.rb
+++ d/bicycle-meter-gnuplot.rb
@@ -16,23 +16,27 @@ open( "|gnuplot", "w" ) do |gnuplot|
    gnuplot.puts "set term png transparent"
    gnuplot.puts "set out 'bicycle-meter.png'"
    gnuplot.puts <<EOF
+	  set title "(Cycling meter)"
 	  set xdata time
 	  set timefmt "%Y-%m-%d"
 	  set format x "%Y-%m-%d"
 	  set xtic rotate
 	  set bmargin 6
 	  set ylabel "Distance (km)"
+	  set y2label "Cumulative Distance (km)"
+	  set y2tics autofreq
 	  set xtics ( #{ monthes.map{|d| %Q["#{d.to_s}"] }.join(",") } )
-	  plot ["#{first_date.to_s}":"#{last_date.to_s}"] "#{DATA_FILE}" \
-		using 1:2 with impulses lw 3 \
-		notitle
+	  plot ["#{first_date.to_s}":"#{last_date.to_s}"] \
+		"#{DATA_FILE}" using 1:2 with impulses lw 3 title "Distance (km)", \
+		"#{DATA_FILE}" using 1:4 axes x1y2 with steps title "Cumulative distance (km)"
 EOF
    # Thumbnail:
    gnuplot.puts <<EOF
   	  set size .4
 	  set out "bicycle-meter-s.png"
-	  plot ["#{first_date.to_s}":"#{last_date.to_s}"] "#{DATA_FILE}" \
-		using 1:2 with impulses lw 3 \
-		notitle
+	  set y2label "Cumulative Distance (km)" 0,-3
+	  plot ["#{first_date.to_s}":"#{last_date.to_s}"] \
+		"#{DATA_FILE}" using 1:2 with impulses lw 3 notitle, \
+		"#{DATA_FILE}" using 1:4 axes x1y2 with steps notitle
 EOF
 end
