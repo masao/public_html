3a93e1bb2f4c865817330bac69302f1d67988a7e
masao <tmasao@acm.org>
2010-03-21 06:27:43 +0000

    changed to use ' instead of ", in order to avoid \u-like effect in "".

--- software/twitter2cl.rb
+++ software/twitter2cl.rb
@@ -22,5 +22,5 @@ twitter.timeline_for( :me, :count => 200 ) do |status|
       puts "\t* [[今日のつぶやき|http://twitter.com/#{ username }]]:"
    end
    time_s = status.created_at.strftime("%H:%M")
-   puts "\t{{twitter \"#{username}\", \"#{ time_s }\", \"#{ status.text.gsub(/[\"\@]/){|e| "\\#{e}" } }\", #{ status.id }, '#{ status.source }', '#{ status.in_reply_to_status_id }', '#{ status.in_reply_to_screen_name }' }}"
+   puts "\t{{twitter '#{username}', '#{ time_s }', '#{ status.text.gsub( /\'/, "\\'" ) }', #{ status.id }, '#{ status.source }', '#{ status.in_reply_to_status_id }', '#{ status.in_reply_to_screen_name }' }}"
 end
