d0a017ab23279404ce4d3e978e4d7b7a39e70a09
masao <tmasao@acm.org>
2013-10-12 14:23:14 +0000

    changed to use OAuth.

--- software/twitter_save_user_timeline.rb
+++ software/twitter_save_user_timeline.rb
@@ -1,22 +1,47 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 # $Id$
 
-require "uri"
-require "open-uri"
+require "date"
 require "yaml"
 
 require "rubygems"
-require "json"
+require "twitter"
+
+Faraday::Request.lookup_middleware(:multipart)
+
+def load_twitter_db( file = "twitter.yml" )
+   YAML.load( open( file ) ) 
+end
+
+class Hash
+   def new_str_key
+      h = Hash.new
+      self.each do |k, v|
+	 k = k.to_s
+         if v.class == Hash
+	    v = v.new_str_key
+	 end
+	 h[ k ] = v
+      end
+      h
+   end
+end
 
 if $0 == __FILE__
-   FILENAME = "user_timeline.yml"
-   params = ARGV
-   params = [ "screen_name=tmasao" ] if ARGV.empty?
-   params_u = params.join( "&" )
-   #STDERR.puts params_u.inspect
-   url = "http://api.twitter.com/1/statuses/user_timeline.json?#{ params_u }"
-   json = open( url ){|io| io.read }
-   data = JSON.load( json )
-   #p data
-   puts data.to_yaml
+   config_file = "twitter.yml"
+   config = YAML.load( open( config_file ) )["test"]
+   username = config["login"]
+   #p config
+   Twitter.configure do |c|
+      c.consumer_key = config[ "oauth_consumer_token" ]
+      c.consumer_secret = config[ "oauth_consumer_secret" ]
+      c.oauth_token = config[ "oauth_token" ]
+      c.oauth_token_secret = config[ "oauth_token_secret" ]
+   end
+   tweets = Twitter.user_timeline( "tmasao" )
+   tweets = tweets.map do |tweet|
+      tweet.attrs.new_str_key
+   end
+   puts tweets.to_yaml
 end
