50efd756f92e0761cad789f85e2d042fe6728c0d
Masao Takaku <tmasao@acm.org>
2015-05-13 07:38:32 +0000

    add cl2research-meter.

--- software/cl2research-meter.pl
+++ software/cl2research-meter.pl
@@ -7,36 +7,36 @@ use warnings;
 use ChangeLogReader;
 use Data::Dumper;
 
+sub time_parse($) {
+    my ($time) = @_;
+    my ($hh, $mm) = split( /:/, $time );
+    my $minutes = $hh * 60 + $mm;
+    #print "$time => $minutes\n";
+    return $minutes;
+}
+
 my $cl = ChangeLogReader->new();
 for my $fname (@ARGV) {
     $cl->store_changelog_file($fname);
 }
 my $time_total = 0;
 foreach my $ymd (sort keys %{$cl->{all}}) {
-    # print Dumper(\%{$cl->{all}->{$ymd}});
     my $minutes = 0;
     my $entry = $cl->{all}->{$ymd};
     for (my $i = $entry->{curid}; $i >= 1; $i--) {
 	if ($entry->{$i}->{ho} eq '研究') {
 	    my $cont = $entry->{$i}->{co};
-	    #print join( "\t", "<$ymd>", $cont );
-	    if ( $cont =~ /\s*(\[[\d\:\,\s]+)\]/o ) {
+	    if ( $cont =~ /\s*\[([\d\:\,\s\-]+)\]/o ) {
+		#print $1,"\n";
 		my @times = split( /,\s*/, $1 );
 		foreach my $time ( @times ) {
 		    my ( $start, $end ) = split( /\s*\-\s*/, $time );
 		    $minutes += time_parse( $end ) - time_parse( $start );
 		}
 	    }
+	    next if $minutes == 0;
 	    $time_total += $minutes;
-	    $time = "0:".$time if not defined((split(/:/, $time))[2]);
 	    print join( "\t", $ymd, $minutes, $time_total ), "\n";
 	}
     }
-}
-
-sub time_parse($) {
-    my ($time) = @_;
-    my ($hh, $mm) = split( /:/, $time );
-    my $minutes = $hh * 60 + $mm;
-    return $minutes;
 }
